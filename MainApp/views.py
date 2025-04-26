c7a3c38aef2276c596b09f17f431d73443e78482
mblevchenko <mblevchenko@yandex.ru>
2025-04-26 16:22:25 +0300

    M1:Task 1

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def homepage(request):
+    text="""
+    <h1>"Изучаем django"</h1>
+    <strong>Автор</strong>: <i>Левченко М.Б.</i>
+    """
+    return HttpResponse(text)
